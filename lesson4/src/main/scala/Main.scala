b91bde460c2cdff40e4c4097be5c40a79e2f7256
Kenji Yamauchi <k_y@goodenough.jp>
2020-12-28 18:03:22 +0900

    Lesson4

---
+++ lesson4/src/main/scala/Main.scala
@@ -0,0 +1,25 @@
+object Main extends App {
+//  println("Hello, World!")
+
+  var marksSum = 0
+  var marksCount = 0
+
+  def averageMark: Double = marksSum.toDouble / marksCount
+
+  //  def markExam(q1: Double, q2: Double, q3: Double) = Math.round(((q1 + q2 + q3) / 3) * 10 / 3)
+
+  def markExam(q1: Double, q2: Double, q3: Double): Int = {
+    val avgScore = (q1 + q2 + q3) / 3
+    val scaledScore = avgScore * 10 / 3
+    val mark = Math.round(scaledScore).toInt
+
+    marksSum += mark
+    marksCount += 1
+    mark
+  }
+
+//  println("---markExam---")
+//  println(markExam(1.5, 2, 2.5))
+  println("---markExam more readable---")
+  println(markExam(1.5, 2, 2.5))
+}
