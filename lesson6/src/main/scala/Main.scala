75e735a0c2d6ece518c5557425b18051a305fa15
Kenji <yamauchi_ke@mti.co.jp>
2021-01-01 17:02:50 +0900

    Mod : Lesson6

--- lesson6/src/main/scala/Main.scala
+++ lesson6/src/main/scala/Main.scala
@@ -1,3 +1,20 @@
 object Main extends App {
-  println("Hello, World!")
+  // println("Hello, World!")
+
+  def isEven(n: Int): Boolean = {
+    n % 2 == 0
+  }
+
+  println("--- isEven ---")
+  println("--- 123 ----")
+  println(isEven(123))
+  println("--- 1024 ----")
+  println(isEven(1024))
+
+  def pow(exponent: Int, base: Int): Double = {
+    Math.pow(base, exponent)
+  }
+
+  println("--- Lesson6 Try this ---")
+  println(pow(3, 2))
 }
