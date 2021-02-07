5020790596a02faed99c7adae7abf575625f9ed4
Kenji Yamauchi <k_y@goodenough.jp>
2021-02-07 14:56:02 +0900

    mod : Main..scala

--- lesson8/src/main/scala/example/Main.scala
+++ lesson8/src/main/scala/example/Main.scala
@@ -6,9 +6,9 @@ object Main extends App {
 
     val machine = new VendingMachine
  
-    machine.chocolateBar += 2
+    machine.chocolateBar += 1
     machine.granolaBar += 1
-    println(machine.buy("chocolate", 1))
+    println(machine.buy("chocolate", 1.5))
     println(machine.buy("granola", 2))
     println(machine.totalMoney)
 
